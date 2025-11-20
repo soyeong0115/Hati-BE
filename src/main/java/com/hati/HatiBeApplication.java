87891863171aec7dc98d08311b31160731ce2e2e
soyeong <mb535622@sookmyung.ac.kr>
2025-11-20 22:32:13 +0900

    chore: 프로젝트 생성 및 초기 세팅

---
+++ src/main/java/com/hati/HatiBeApplication.java
@@ -0,0 +1,13 @@
+package com.hati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hati/HatiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
