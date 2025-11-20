6ebf0a009f4874740c32e2f7e786095f63d23d85
soyeong <mb535622@sookmyung.ac.kr>
2025-11-20 22:21:54 +0900

    feat: Feedback DTO 생성

---
+++ src/main/java/com/hati/dto/FeedbackRequest.java
@@ -0,0 +1,18 @@
+package com.hati.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FeedbackRequest {
+
+    @NotBlank(message = "의견을 입력해주세요")
+    private String feedback;
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
+    private String email;
+}
